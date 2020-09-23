6b5733efc0d4f2946945fb2b0f1836d74a6eeaa7
Jakob Waibel <jakob.waibel@web.de>
2020-09-23 15:35:34 +0200

    fix: formatting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
-// [x] Fetch API
-// [ ] Make fetching from API a reusable function or get all Data seperately
-// [ ] Create Design Template first
-// [ ] Check how old the data is
-
 function App() {
-  // Check which Datatype is being returned and adjust default value therefor
+
   const [temperatureAverage, setTemperatureAverage] = useState([])
   const [temperatureMax, setTemperatureMax] = useState([])
   const [temperatureMin, setTemperatureMin] = useState([])
@@ -31,8 +26,8 @@ function App() {
       const res = await fetch("https://api.nasa.gov/insight_weather/?api_key=etIV1rEZcwSjjQq1qreyKogMVfTFsIgkMN1QhG6u&feedtype=json&ver=1.0")
       const data = await res.json()
 
-      setTemperatureAverage(data[data.sol_keys[6]].AT.av) // enter value for temperature here
-      setTemperatureMax(data[data.sol_keys[6]].AT.mx) 
+      setTemperatureAverage(data[data.sol_keys[6]].AT.av)
+      setTemperatureMax(data[data.sol_keys[6]].AT.mx)
       setTemperatureMin(data[data.sol_keys[6]].AT.mn)
       setTemperatureSamples(data[data.sol_keys[6]].AT.ct)
       setSol(data.sol_keys[6])
@@ -56,7 +51,7 @@ function App() {
 
 
   return (
-  <div><h1>Elysium Planitia</h1><h1>Sol: {sol} </h1><h1>Average temperature: {temperatureAverage} °F</h1><h1>Max temperature: {temperatureMax} °F</h1><h1>Min temperature: {temperatureMin}°F</h1><h1>Number of temperature-samples this sol: {temperatureSamples}</h1><h1>First_UTC: {first_UTC}</h1><h1>Last_UTC: {last_UTC}</h1><h1>Season: {season}</h1><h1>Average pressure: {pressureAverage} hPa</h1><h1>Max pressure: {pressureMax} hPa</h1><h1>Min pressure: {pressureMin} hPa</h1><h1>Number of pressure-samples this sol: {pressureSamples}</h1><h1>Average windspeed: {windspeedAverage} m/s</h1><h1>Max windspeed: {windspeedMax} m/s</h1><h1>Min windspeed: {windspeedMin} m/s</h1><h1>Number of windspeed samples this sol: {windspeedSamples}</h1></div>
+    <div><h1>Elysium Planitia</h1><h1>Sol: {sol} </h1><h1>Average temperature: {temperatureAverage} °F</h1><h1>Max temperature: {temperatureMax} °F</h1><h1>Min temperature: {temperatureMin}°F</h1><h1>Number of temperature-samples this sol: {temperatureSamples}</h1><h1>First_UTC: {first_UTC}</h1><h1>Last_UTC: {last_UTC}</h1><h1>Season: {season}</h1><h1>Average pressure: {pressureAverage} hPa</h1><h1>Max pressure: {pressureMax} hPa</h1><h1>Min pressure: {pressureMin} hPa</h1><h1>Number of pressure-samples this sol: {pressureSamples}</h1><h1>Average windspeed: {windspeedAverage} m/s</h1><h1>Max windspeed: {windspeedMax} m/s</h1><h1>Min windspeed: {windspeedMin} m/s</h1><h1>Number of windspeed samples this sol: {windspeedSamples}</h1></div>
   )
 }
 
