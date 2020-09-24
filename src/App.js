7b4e0b4a16f58112ee2df02fcb2a1cf674ef45f9
Jakob Waibel <jakob.waibel@web.de>
2020-09-24 17:16:15 +0200

    feat: add rechart graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import Data from "./Components/Data"
-import './App.css';
+import React from "react";
+import Data from "./Components/Data";
+import "./App.css";
+import Graph from "./Components/Graph";
 
 function App() {
-
   // [ ] Store data to make charts
-  // [ ] Style data component  
-  // [ ] Create graph component 
-  // [ ] Create prediction through interpolation 
+  // [ ] Distinguish into easy and advanced mode
+  // [ ] Create Component for swap to advanced mode on the bottom of the other modes onClick
+  // [x] Style data component
+  // [ ] Create graph component
+  // [ ] Create prediction through interpolation
   // [ ] Style overall website
+  // [ ] publish Web-App and connect with Website
+  // [ ] convert between Fahrenheit, Celsius, Kelvin
 
-  const numbers = [6,5,4,3,2,1,0]
+  const numbers = [6, 5, 4, 3, 2, 1, 0];
 
-  const listData = numbers.map((number) => <Data dayIndex={number}/>)
-  
+  const listData = numbers.map((number, index) => (
+    <Data dayIndex={number} key={index} />
+  ));
+
   return (
-
-    listData
-    
-  )
+    <>
+      {listData}
+      <Graph />
+    </>
+  );
 }
 export default App;
-
