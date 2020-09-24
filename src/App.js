4f235fe575319e1e3b1ddc1bf815f1fc2b20e92a
Jakob Waibel <jakob.waibel@web.de>
2020-09-24 15:08:48 +0200

    feat: creating stylesheet, simplifying data in UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Data from "./Components/Data"
 import './App.css';
 
-
-// [ ] make component to fetch Data
 function App() {
 
-  const numbers = [0,1,2,3,4,5,6]
+  // [ ] Store data to make charts
+  // [ ] Style data component  
+  // [ ] Create graph component 
+  // [ ] Create prediction through interpolation 
+  // [ ] Style overall website
+
+  const numbers = [6,5,4,3,2,1,0]
 
   const listData = numbers.map((number) => <Data dayIndex={number}/>)
   
--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,29 +1,29 @@
-import React,  {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 
-function Data({dayIndex}) {
+function Data({ dayIndex }) {
 
-    if (dayIndex > 6 || dayIndex < 0) {
-        console.log("Please enter a value between 0 and 6")
-    }
+  if (dayIndex > 6 || dayIndex < 0) {
+    console.log("Please enter a value between 0 and 6")
+  }
 
-    const [temperatureAverage, setTemperatureAverage] = useState([])
-    const [temperatureMax, setTemperatureMax] = useState([])
-    const [temperatureMin, setTemperatureMin] = useState([])
-    const [temperatureSamples, setTemperatureSamples] = useState([])
-    const [sol, setSol] = useState(0)
-    const [first_UTC, setFirst_UTC] = useState("")
-    const [last_UTC, setLast_UTC] = useState("")
-    const [season, setSeason] = useState("")
-    const [pressureAverage, setPressureAverage] = useState([])
-    const [pressureMax, setPressureMax] = useState([])
-    const [pressureMin, setPressureMin] = useState([])
-    const [pressureSamples, setPressureSamples] = useState([])
-    const [windspeedAverage, setWindSpeedAverage] = useState([])
-    const [windspeedMax, setWindSpeedMax] = useState([])
-    const [windspeedMin, setWindSpeedMin] = useState([])
-    const [windspeedSamples, setWindspeedSamples] = useState([])
+  const [temperatureAverage, setTemperatureAverage] = useState([])
+  const [temperatureMax, setTemperatureMax] = useState([])
+  const [temperatureMin, setTemperatureMin] = useState([])
+  const [temperatureSamples, setTemperatureSamples] = useState([])
+  const [sol, setSol] = useState(0)
+  //const [first_UTC, setFirst_UTC] = useState("")
+  //const [last_UTC, setLast_UTC] = useState("")
+  const [season, setSeason] = useState("")
+  const [pressureAverage, setPressureAverage] = useState([])
+  const [pressureMax, setPressureMax] = useState([])
+  const [pressureMin, setPressureMin] = useState([])
+  const [pressureSamples, setPressureSamples] = useState([])
+  const [windspeedAverage, setWindSpeedAverage] = useState([])
+  const [windspeedMax, setWindSpeedMax] = useState([])
+  const [windspeedMin, setWindSpeedMin] = useState([])
+  const [windspeedSamples, setWindspeedSamples] = useState([])
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("https://api.nasa.gov/insight_weather/?api_key=etIV1rEZcwSjjQq1qreyKogMVfTFsIgkMN1QhG6u&feedtype=json&ver=1.0")
       const data = await res.json()
@@ -33,8 +33,8 @@ function Data({dayIndex}) {
       setTemperatureMin(data[data.sol_keys[dayIndex]].AT.mn)
       setTemperatureSamples(data[data.sol_keys[dayIndex]].AT.ct)
       setSol(data.sol_keys[dayIndex])
-      setFirst_UTC(data[data.sol_keys[dayIndex]].First_UTC)
-      setLast_UTC(data[data.sol_keys[dayIndex]].Last_UTC)
+      //setFirst_UTC(data[data.sol_keys[dayIndex]].First_UTC)
+      //setLast_UTC(data[data.sol_keys[dayIndex]].Last_UTC)
       setSeason(data[data.sol_keys[dayIndex]].Season)
       setPressureAverage(data[data.sol_keys[dayIndex]].PRE.av)
       setPressureMax(data[data.sol_keys[dayIndex]].PRE.mx)
@@ -49,24 +49,33 @@ function Data({dayIndex}) {
     fetchData()
   }, [dayIndex]);
 
-   return (
+  return (
     // Design basic component below:
-    <div>
-      <h1>Elysium Planitia</h1>
-      <h1>Sol: {sol} </h1><h1>Average temperature: {temperatureAverage} °F</h1>
-      <h1>Max temperature: {temperatureMax} °F</h1>
-      <h1>Min temperature: {temperatureMin}°F</h1>
-      <h1>Number of temperature-samples this sol: {temperatureSamples}</h1>
-      <h1>First_UTC: {first_UTC}</h1>
-      <h1>Last_UTC: {last_UTC}</h1><h1>Season: {season}</h1>
-      <h1>Average pressure: {pressureAverage} hPa</h1>
-      <h1>Max pressure: {pressureMax} hPa</h1>
-      <h1>Min pressure: {pressureMin} hPa</h1>
-      <h1>Number of pressure-samples this sol: {pressureSamples}</h1>
-      <h1>Average windspeed: {windspeedAverage} m/s</h1>
-      <h1>Max windspeed: {windspeedMax} m/s</h1>
-      <h1>Min windspeed: {windspeedMin} m/s</h1>
-      <h1>Number of windspeed samples this sol: {windspeedSamples}</h1>
+    <div className="main">
+      <h1 className="sol">Sol: {sol} </h1>
+      <h2>Elysium Planitia, {season}</h2>
+      
+      <div className="divBox">
+
+        <div className="container">
+          <h2 className="average">{Math.round(temperatureAverage)} °F</h2>
+          <h2 className="maxMin">{Math.round(temperatureMax)}° / {Math.round(temperatureMin)}°</h2>
+          <h2>Samples: {temperatureSamples}</h2>
+        </div>
+
+        <div className="container">
+          <h2 className="average">{Math.round(pressureAverage)} hPa</h2>
+          <h2 className="maxMin">{Math.round(pressureMax)} hPa / {Math.round(pressureMin)} hPa</h2>
+          <h2>Samples: {pressureSamples}</h2>
+        </div>
+
+        <div className="container">
+          <h2 className="average">{Math.round(windspeedAverage)} m/s</h2>
+          <h2 className="maxMin">{Math.round(windspeedMax)} m/s / {Math.round(windspeedMin)} m/s</h2>
+          <h2>Samples: {windspeedSamples}</h2>
+        </div>
+
+      </div>
     </div>
 
   )
