487b793b76cb4d829a3ad079611f7e11f72ed7f2
Jakob Waibel <jakob.waibel@web.de>
2020-09-26 15:49:44 +0200

    fix: mobile responsiveness

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export const Filter = ({ format, onChange }) => (
-    <select selected={format} onChange={(e) => onChange(e.target.value)}>
-      <option selected value="F">Fahrenheit</option>
-      <option value="C">Celsius</option>
-      <option value="K">Kelvin</option>
-    </select>
-  );
+  <select value={format} onChange={(e) => onChange(e.target.value)}>
+    <option value="F">Fahrenheit</option>
+    <option value="C">Celsius</option>
+    <option value="K">Kelvin</option>
+  </select>
+);
 
 export default Filter;
