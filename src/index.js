a082328a608ba8fa180825b1291cb883bffffa0a
Jakob Waibel <jakob.waibel@web.de>
2021-02-12 17:20:21 +0100

    refactor: Remove firebase

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import firebase from "firebase";
+// import firebase from "firebase";
 
-firebase.initializeApp({
-  apiKey: "91MejiYzInH091RnX4LXjuYjyJmZCt7JN7G73Yu1",
-  authDomain: "limwa-6200b.firebaseapp.com",
-  databaseURL: "https://limwa-6200b.firebaseio.com",
-  projectId: "limwa-6200b",
-  storageBucket: "limwa-6200b.appspot.com",
-  messagingSenderId: "877129690500",
-  appId: "1:877129690500:web:5e8bb06db9c60a9e1d81d1",
-  measurementId: "G-G8KSC8WVVT",
-});
+// firebase.initializeApp({
+//   apiKey: "91MejiYzInH091RnX4LXjuYjyJmZCt7JN7G73Yu1",
+//   authDomain: "limwa-6200b.firebaseapp.com",
+//   databaseURL: "https://limwa-6200b.firebaseio.com",
+//   projectId: "limwa-6200b",
+//   storageBucket: "limwa-6200b.appspot.com",
+//   messagingSenderId: "877129690500",
+//   appId: "1:877129690500:web:5e8bb06db9c60a9e1d81d1",
+//   measurementId: "G-G8KSC8WVVT",
+// });
 
 ReactDOM.render(
   <React.StrictMode>
